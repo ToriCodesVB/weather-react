462453169eb9468fcd4ff922c5a041a5013c71a5
Viktoriia <117581411+ToriCodesVB@users.noreply.github.com>
2023-05-15 21:55:49 +0300

    Added axios integration so it shows the weather of a city

---
+++ src/Weather.js
@@ -0,0 +1,27 @@
+import React from "react";
+import axios from "axios";
+import { Puff } from "react-loader-spinner";
+
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = "5b6d1c8a353dfb90cc68b19a2a2c1017";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Puff
+      height="80"
+      width="80"
+      radius={1}
+      color="#4fa94d"
+      ariaLabel="puff-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
+}
