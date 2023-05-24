4006f12f4cfd680349e2312978ee581102b9738d
Viktoriia <117581411+ToriCodesVB@users.noreply.github.com>
2023-05-24 08:45:33 +0300

    Update index.html

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <h1>WeatherApp </h1>
       <footer>
         This project was created by Viktoriia Baburkina and is{" "}
-        <a href="https://github.com/ToriCodesVB/weather-react" target="_blank">opened-sourced on GitHub</a>
+        <a href="https://github.com/ToriCodesVB/weather-react" target="_blank" rel="noreferrer">opened-sourced on GitHub</a>
       </footer>
     </div>
   );
