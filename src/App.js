fd1a5d354f9f11aed5d93566d571e565212bd06c
Viktoriia <117581411+ToriCodesVB@users.noreply.github.com>
2023-05-26 01:15:21 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 
-
-
 function App() {
   return (
     <div className="App">
-      <h1>WeatherApp </h1>
-      <footer>
-        This project was created by Viktoriia Baburkina and is{" "}
-        <a href="https://github.com/ToriCodesVB/weather-react" target="_blank" rel="noreferrer">opened-sourced on GitHub</a>
-      </footer>
+      <div className="container">
+        <h1>WeatherApp </h1>
+        <footer>
+          This project was created by Viktoriia Baburkina and is{" "}
+          <a
+            href="https://github.com/ToriCodesVB/weather-react"
+            target="_blank"
+            rel="noreferrer">
+            opened-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
